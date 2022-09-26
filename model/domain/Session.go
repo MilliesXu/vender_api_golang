4c87757c21d89b65c40b5a0f3558c038441febcb
MilliesXu <winzchip@gmail.com>
2022-09-26 22:09:04 +0700

    Change session field

--- model/domain/Session.go
+++ model/domain/Session.go
@@ -3,9 +3,9 @@ package domain
 import "time"
 
 type Session struct {
-	Id         int
-	User       map[string]string
-	Valid      bool
-	Created_at time.Time
-	Updated_at time.Time
+	Id        int
+	User      map[string]string
+	Valid     bool
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
